d2b77c8933e615901c39fd148f4efa18340b2e9c
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-26 00:49:56 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  const p = $('p');
+  return p;
+}
+
+function lastImageSelector(){
+  const lastImg = $('img:last');
+  return lastImg;
+}
+
+function ninjaBabySelector() {
+  const nb = $('img#baby-ninja');
+  return nb;
+}
+
+function divSelector() {
+  const pics = $('div.pics');
+  return pics;
+}
+
+function firstListItem() {
+  const first = $("ul:first-child[id='pic-list']");
+  return first;
+}
